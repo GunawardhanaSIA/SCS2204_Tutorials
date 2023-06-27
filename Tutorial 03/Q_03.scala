1dc2bca357443afa5cebc08377320d32072946d6
GunawardhanaSIA <135797613+GunawardhanaSIA@users.noreply.github.com>
2023-06-27 08:01:46 +0530

    Uploaded question 03

--- Tutorial 03/Q_03.scala
+++ Tutorial 03/Q_03.scala
@@ -1,16 +1,18 @@
 object Q_03 {
   def main(args: Array[String]): Unit = {
     printf("Enter the num1 = ")
-    var num1 = scala.io.StdIn.readDouble()
+    var num1 = scala.io.StdIn.readInt()
 
     printf("Enter the num2 = ")
-    var num2 = scala.io.StdIn.readDouble()
+    var num2 = scala.io.StdIn.readInt()
 
-    printf("Average = %.2f", average(num1, num2))
+    var num3 = num2.toDouble
+
+    printf("Average = %.2f", average(num1, num3))
   }
 
-  def average(num1: Double, num2: Double): Double = {
-    (num1 + num2) / 2
+  def average(num1: Int, num3: Double): Double = {
+    (num1 + num3) / 2
   }
 
 }
